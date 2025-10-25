124e95ec991cbd1e920c2d18c313c03a266c0190
galihwibii <galihwbsn04@gmail.com>
2025-10-25 12:38:37 +0700

    nambah main cpp galih

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,24 @@
 // #define PROJECT_HILMAN
 // #define PROJECT_MERLIN
 #define PROJECT_GALIH
+// #define PROJECT_GALIH2
 // #define PROJECT_FARREL
 // #define PROJECT_UNKNOWN
 
+#include "project-control.h"
+
 void setup() {
-  
+  #if defined(PROJECT_GALIH)
+  setupProjectGalih();
+  #elif defined(PROJECT_GALIH2)
+  setupProjectGalih2();
+  #endif
 }
 
 void loop() {
-  
+  #if defined(PROJECT_GALIH)
+  loopProjectGalih();
+  #elif defined(PROJECT_GALIH2)
+  loopProjectGalih2();
+  #endif
 }
